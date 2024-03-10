3d097bd3098057b89a8d09d5a3feb169a7a43712
julie <julie.gysel@gmail.com>
2024-03-10 11:58:38 +0100

    Homepage beginnings

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Button } from 'primereact/button';
+import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
-  return <h2>Home</h2>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={'flex align-items-center flex-column p-6'}>
+      <h1 className={'text-center text-3xl md:text-6xl  xl:text-7xl'}>
+        [Reservation App]
+        <br />
+        Sharing made easy {/* is this too on the nose? */}
+      </h1>
+      <p>Set up a resource. Send out a link. Manage the reservations.</p>
+      <Button raised size="large" onClick={() => navigate('create')}>
+        Share something
+      </Button>
+      {/* </div> */}
+    </div>
+  );
 };
