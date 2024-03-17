94238f67ee560720d63e746e31b52459b9da0d60
julie <julie.gysel@gmail.com>
2024-03-17 21:20:28 +0100

    Add Formik to the Create resource form

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,7 @@
 export * from './Navbar';
 export * from './InputField';
+export * from './TextAreaField';
+export * from './SelectButtonsField';
+export * from './CheckboxField';
+export * from './NumberField';
+export * from './CalendarField';
